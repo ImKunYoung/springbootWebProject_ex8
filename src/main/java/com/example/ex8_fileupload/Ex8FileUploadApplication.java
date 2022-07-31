cdf861fd7cd577e29fdb5ee03e66f98fea6bf0b8
ImKunYoung <lim8667@gmail.com>
2022-07-31 13:50:12 +0900

    first commit

---
+++ src/main/java/com/example/ex8_fileupload/Ex8FileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.example.ex8_fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex8FileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex8FileUploadApplication.class, args);
+    }
+
+}
