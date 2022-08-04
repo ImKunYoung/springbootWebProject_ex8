556eb64e0823596d75427f9cc1e5489a9e2eaae9
ImKunYoung <lim8667@gmail.com>
2022-08-04 15:15:04 +0900

    로그 추가

--- src/main/java/com/example/ex8_fileupload/controller/UploadTestController.java
+++ src/main/java/com/example/ex8_fileupload/controller/UploadTestController.java
@@ -1,13 +1,19 @@
 package com.example.ex8_fileupload.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Log4j2
 public class UploadTestController {
 
     @GetMapping("/uploadEx")
     public void uploadEx() {
+
+        log.info("uploadEx");
+
     }
 
 }
+
