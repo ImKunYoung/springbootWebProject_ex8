3d3acb46d469e734e7fd39f49e28f523d2bdef56
ImKunYoung <lim8667@gmail.com>
2022-08-06 23:51:30 +0900

    날짜 폴더 생성

--- src/main/java/com/example/ex8_fileupload/controller/UploadController.java
+++ src/main/java/com/example/ex8_fileupload/controller/UploadController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @Log4j2
 public class UploadController {
@@ -24,13 +28,37 @@ public class UploadController {
                 return;
             }
 
+            // 실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String orginalName = uploadFile.getOriginalFilename();
             assert orginalName != null;
             String fileName = orginalName.substring(orginalName.lastIndexOf("\\") + 1);
 
             log.info("fileName: "+fileName);
 
+            // 날짜 폴더 생성
+            String folderPath = makeFolder();
+
+
+
         }
+
+    }
+
+    /*날짜 폴더 생성*/
+    private String makeFolder() {
+
+        String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+
+        String folderPath = str.replace("/", File.separator);
+
+        // make folder --------
+        File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if(uploadPathFolder.exists() == false) {
+            uploadPathFolder.mkdirs();
+        }
+
+        return folderPath;
 
     }
 
