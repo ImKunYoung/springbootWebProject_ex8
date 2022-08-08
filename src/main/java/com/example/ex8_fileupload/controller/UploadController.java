14016101ffbd12d8a1c539b5354175652be88d1c
ImKunYoung <lim8667@gmail.com>
2022-08-08 18:15:25 +0900

    업로드 결과 반환과 화면 처리

--- src/main/java/com/example/ex8_fileupload/controller/UploadController.java
+++ src/main/java/com/example/ex8_fileupload/controller/UploadController.java
@@ -3,14 +3,20 @@ package com.example.ex8_fileupload.controller;
 import com.example.ex8_fileupload.dto.UploadResultDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -69,6 +75,40 @@ public class UploadController {
 
         }
         return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
+
+    }
+
+    /*업로드 이미지 출력하기*/
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+
+        ResponseEntity<byte[]> result;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, StandardCharsets.UTF_8);
+
+            log.info("fileName: " + srcFileName);
+
+            File file = new File(uploadPath + File.separator + srcFileName);
+
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+
+            // MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+
+
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return result;
 
     }
 
