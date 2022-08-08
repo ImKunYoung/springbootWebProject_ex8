d5efdf6ab65c0ec708e7f58c7252a3436ead611f
ImKunYoung <lim8667@gmail.com>
2022-08-08 17:26:47 +0900

    업로드 결과 반환과 화면 처리

---
+++ src/main/java/com/example/ex8_fileupload/dto/UploadResultDTO.java
@@ -0,0 +1,23 @@
+package com.example.ex8_fileupload.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Data
+@AllArgsConstructor
+public class UploadResultDTO implements Serializable {
+
+    private String fileName;
+    private String uuid;
+    private String folderPath;
+
+    public String getImageURL() {
+        return URLEncoder.encode(folderPath+"/"+uuid+"_"+fileName, StandardCharsets.UTF_8);
+    }
+
+}
